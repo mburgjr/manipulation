2942ef3026f446ba199ea54b7ee9951031bb6319
Russ Tedrake <russt@mit.edu>
2022-11-27 12:43:19 -0500

    WIP: gcs clutter clearing

---
+++ manipulation/iris.py
@@ -0,0 +1,11 @@
+
+class IrisBuilder():
+    def __init__(self, plant, context, iris_options):
+        self._plant = plant
+        self._context = context
+        self._iris_options = iris_options
+        self._regions = []
+
+    def AddRegion(use_existing_regions_as_obstacles=True):
+        print("todo")
+    
